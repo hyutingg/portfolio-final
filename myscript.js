24ef98910ffc5d700525e1f368b964c485b63cff
Amanda Hsu <hyuting@umich.edu>
2021-12-09 15:47:50 -0500

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,12 @@
+function show_overlay(x) {
+	x.parentElement.parentElement.style.opacity = "100%";
+  }
+  function show_overlay_gallery(x) {
+	x.parentElement.parentElement.parentElement.style.opacity = "100%";
+  }
+  function hide_overlay(x) {
+	x.parentElement.parentElement.style.opacity = "0%";
+  }
+  function hide_overlay_gallery(x) {
+	x.parentElement.parentElement.parentElement.style.opacity = "0%";
+  }
